5e1b613a447c77e724cc4f3ba841ffceff4942c2
StartAutomating <james.brundage@start-automating.com>
2026-03-10 17:50:29 -0700

    feat: Deploying workflows ( Fixes #23 )
    Also documenting deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -13,42 +13,80 @@
     Feel free to copy, paste, and modify this as needed.
 
     Ideally, please modify the notes to what this script does.
-
-    For example, this script is _really_ simple.
-
-    In this case we're building a single page site.
     
-    This means this script is _very_ short.
+    ---    
+
+    In this site, we're making some xrpc endpoints to restfully expose data.
+
+    Then, we're making a page for this organization.
+
+    This should work for any GitHub organization.
 #>
 param()
 
+# Make sure we're in the current location
 if ($PSScriptRoot) { Push-Location $psScriptRoot }
 
-$xrpcData = [Ordered]@{}
+# We'll cache our xrpc data first
+# Fun fact: this becomes our site data.
+$site = [Ordered]@{}
 
+# We'll call each script that generates xrpc an "indexer"
+# (because it generates an index of the content)
+# Any `*.*.*.ps1` beneath /xrpc/ will be conisdered an indexer.
 foreach ($xrpcIndexFile in 
     Get-ChildItem -Path $psScriptRoot -Filter xrpc | 
     Get-ChildItem -filter *.*.*.ps1 
 ) {
+    # Let's get the script
     $xrpcScript = Get-Command $xrpcIndexFile.FullName
+
+    # and run it in the current scope.
     $xrpcOutput = . $xrpcScript
 
-    $xrpcNsid = $xrpcScript.Name -replace '\.ps1$' 
-
+    # To get the NSID, we just need to remove the extension.
+    $xrpcNsid = $xrpcScript.Name -replace '\.ps1$'
+    
+    # Once we know the NSID, we can start to construct the directory.
     $xrpcOutputDirectory = Join-Path $xrpcIndexFile.Directory (
         $xrpcNsid
     )
     
+    # Fun fact number #2:
+    # We can use an index.json file to return static json.
+    # This eliminates most of the server side load. 
     $xrpcOutputFile = Join-Path $xrpcOutputDirectory "index.json"
+
+    # All we have to do is cache our results into a json file
+    # and now our site can serve them up.
     New-Item -ItemType File -Path $xrpcOutputFile -Value (
         $xrpcOutput | ConvertTo-Json -Depth 10
     ) -Force
     
-    $xrpcData[$xrpcNsid] = $xrpcOutput
+    # Before we move onto the next indexer,
+    # let's save our site data.
+    $site[$xrpcNsid] = $xrpcOutput
 }
 
 # All we need to do is run one file and redirect it's output:
 . ./index.html.ps1 > ./index.html
 Get-Item -Path ./index.html
+
+# This site is a useful living example of how to make sites free of frameworks
+# So let's make any workflows we use to build it available on the page.
+
+# First get our workflows
+$gitHubWorkflows = Get-ChildItem -Path .github -Force | 
+    Where-Object Name -EQ workflows
+
+# and if we had any
+if ($gitHubWorkflows) {
+    # make a ./workflows directory if we need to
+    if (-not (Test-Path './workflows')) {
+        New-Item -ItemType Directory -Path ./workflows -Force
+    }
+    # copy them to ./workflows.
+    $gitHubWorkflows | Copy-Item -Destination ./workflows -PassThru
+}
 
 if ($PSScriptRoot) { Pop-Location }
