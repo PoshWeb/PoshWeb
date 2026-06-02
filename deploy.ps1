052fcb0f0e69dca19a31e5110dc6cdd92a1cc0cc
James Brundage <+@noreply.github.com>
2026-06-01 22:53:57 -0700

    feat(xrpc): https://poshweb.org/xrpc/ ( Fixes #31 )

--- deploy.ps1
+++ deploy.ps1
@@ -56,47 +56,17 @@ $cpuSpeed =
 #endregion Clock Speed
 
 #region index data in `/xrpc/`
-
-# We want to run any script in `/xrpc/`.
-
-# Fun fact: this becomes our site data.
-
-# We'll call each script that generates xrpc an "indexer"
-# (because it generates an index of the content)
-# Any `*.*.*.ps1` beneath /xrpc/ will be conisdered an indexer.
-foreach ($xrpcIndexFile in 
-    Get-ChildItem -Path $psScriptRoot -Filter xrpc | 
-    Get-ChildItem -filter *.*.*.ps1 
-) {
-    # Let's get the script
-    $xrpcScript = Get-Command $xrpcIndexFile.FullName
-
-    # and run it in the current scope.
-    $xrpcOutput = . $xrpcScript
-
-    # To get the NSID, we just need to remove the extension.
-    $xrpcNsid = $xrpcScript.Name -replace '\.ps1$'
-    
-    # Once we know the NSID, we can start to construct the directory.
-    $xrpcOutputDirectory = Join-Path $xrpcIndexFile.Directory (
-        $xrpcNsid
+$xrpcFile = (
+    Join-Path $PSScriptRoot 'xrpc' | 
+        Join-Path -ChildPath xrpc.ps1
+)
+if (Test-Path $xrpcFile) {
+    . $xrpcFile > (
+        $xrpcFile -replace '[\\/]xrpc.ps1$', 'index.html'
     )
-    
-    # Fun fact number #2:
-    # We can use an index.json file to return static json.
-    # This eliminates most of the server side load. 
-    $xrpcOutputFile = Join-Path $xrpcOutputDirectory "index.json"
-
-    # All we have to do is cache our results into a json file
-    # and now our site can serve them up.
-    New-Item -ItemType File -Path $xrpcOutputFile -Value (
-        $xrpcOutput | ConvertTo-Json -Depth 10
-    ) -Force
-    
-    # Before we move onto the next indexer,
-    # let's save our site data.
-    $site[$xrpcNsid] = $xrpcOutput
 }
+
+$org = $site['com.github.api.orgs.org']
 
 #endregion index data in `/xrpc/`
 
